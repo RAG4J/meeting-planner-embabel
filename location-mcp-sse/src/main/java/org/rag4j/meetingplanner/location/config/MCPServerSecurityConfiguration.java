e56fd9b5eafe01266a2673b500fcf2e58a8072c9
Jettro Coenradie <jettro@coenradie.com>
2025-09-17 00:18:50 +0200

    All the stuff is secured now

---
+++ location-mcp-sse/src/main/java/org/rag4j/meetingplanner/location/config/MCPServerSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package org.rag4j.meetingplanner.location.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class MCPServerSecurityConfiguration {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+//                .oauth2ResourceServer(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
